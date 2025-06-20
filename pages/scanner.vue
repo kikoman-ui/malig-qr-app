7f52431bd73991ce18ceb70996ee3004aead6e80
Jhin06 <jinsuarez6@gmail.com>
2025-06-20 16:16:13 +0800

    update page

--- pages/scanner.vue
+++ pages/scanner.vue
@@ -1,9 +1,17 @@
 <template>
     <div>
-        <h1>scanner</h1>
-    <v-btn color="primary"
-  elevation="2"
-></v-btn>
+        <v-card>
+            <v-card-text>
+                <v-form>
+                    <v-text-field
+                    outlined
+                    label="|enter number"
+                    type = "number"
+                    v-model = "num2"
+                    ></v-text-field>
+                </v-form>
+            </v-card-text>
+        </v-card>
     </div>
     
 </template>
