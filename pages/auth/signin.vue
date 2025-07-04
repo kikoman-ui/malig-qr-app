9669105bcb0b3fc0fc43884a851f7aadebcfa956
Jhin06 <jinsuarez6@gmail.com>
2025-07-04 18:08:46 +0800

    commit mesage

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -1,22 +1,31 @@
 <template>
     <div class="d-flex align-center justify-center fill-height">
       <v-card width="400" elevation="10" class="pa-4">
+
+        
+      <div class="d-flex justify-center mb-4">
         <v-img
         src="/logo.png"
         height="50"
         width="50"
-        >
-        </v-img>
-        <h2 class="text-center ">Sign In</h2>
+        contain
+        ></v-img>
+      </div>
+ 
+
+        <h2 class="text-center ">Login Page</h2>
         <v-card-text>
           <v-form>
             <v-text-field
+              v-model="username"
               outlined
               label="Username"
               type="text"
               prepend-inner-icon="mdi-account"
             ></v-text-field>
+
             <v-text-field
+              v-model="password"
               outlined
               label="Password"
               type="password"
@@ -24,15 +33,67 @@
             ></v-text-field>
           </v-form>
         </v-card-text>
+
         <v-card-actions class="pt-4">
-          <v-btn color="primary" block large>Sign In</v-btn>
+          <v-btn color="primary" block large @click="handleLogin">Login</v-btn>
         </v-card-actions>
+
+        <!-- Divider -->
+      <div class="text-center my-4">
+        <v-divider></v-divider>
+        <span class="overline grey--text">OR</span>
+        <v-divider></v-divider>
+      </div>
+
+      <!-- Google Sign-In Button -->
+      <v-card-actions>
+        <v-btn
+          block
+          color="white"
+          class="google-btn"
+          elevation="2"
+        >
+          <v-icon left color="red darken-1">mdi-google</v-icon>
+          Sign in with Google
+        </v-btn>
+      </v-card-actions>
+
       </v-card>
+
+      <!-- Snackbar -->
+       <v-snackbar
+       v-model="snackbar"
+       :color="snackbarColor"
+       timeout="3000"
+       top
+       >
+      {{ snackbarText }}
+       </v-snackbar>
+
     </div>
   </template>
   
 <script>
 export default {
-    layout: "auth"
+    layout: "auth",
+    data() {
+      return{
+        username: '',
+        password: '',
+        snackbar: false,
+        snackbarText: '',
+        snackbarColor: 'error'
+      };
+    },
+    methods: {
+      handleLogin() {
+        if (!this.username || !this.password){
+          this.snackbarText = 'Please fill in all fields.';
+          this.snackbarColor = "error";
+          this.snackbar = true;
+          return;
+        }
+      }
+    }
 }
 </script>
